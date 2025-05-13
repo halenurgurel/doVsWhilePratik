643c9f7b2a0b70e555278306bb304324f9f438eb
halenurgurel <halenur.gurel@hotmail.com>
2025-05-13 19:01:00 +0300

    Add project files.

---
+++ doVsWhilePratik/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Console.Write("Limit değerini giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+int sayac = 0;
+
+while (sayac < limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}*/
+
+Console.Write("Limit değerini giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+int sayac = 0;
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+} while (sayac < limit);
