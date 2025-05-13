91dba9853ca365dce7af20077e68c5810ade4361
halenurgurel <halenur.gurel@hotmail.com>
2025-05-13 19:03:50 +0300

    updated

--- doVsWhilePratik/Program.cs
+++ doVsWhilePratik/Program.cs
@@ -1,4 +1,4 @@
-﻿/*Console.Write("Limit değerini giriniz: ");
+﻿/*Console.Write("Limit değerini giriniz: "); 
 int limit = Convert.ToInt32(Console.ReadLine());
 
 int sayac = 0;
@@ -19,3 +19,10 @@ do
     Console.WriteLine("Ben bir Patika'lıyım");
     sayac++;
 } while (sayac < limit);
+
+// FARK:
+// while döngüsünde koşul en başta kontrol edilir. Eğer limit -5 gibi bir negatif sayıysa, döngüye hiç girilmez.
+// do-while döngüsünde ise döngü en az 1 kez çalışır çünkü koşul sona bırakılmıştır.
+// Örneğin limit = -5 verilirse:
+//   while → hiç yazdırmaz.
+//   do-while → 1 kez "Ben bir Patika'lıyım" yazar.
