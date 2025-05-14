668e1669e8b68601dae2a0f438dbf57c9ebb25c2
halenurgurel <halenur.gurel@hotmail.com>
2025-05-14 15:35:26 +0300

    updated.

--- doVsWhilePratik/Program.cs
+++ doVsWhilePratik/Program.cs
@@ -1,24 +1,30 @@
-﻿/*Console.Write("Limit değerini giriniz: "); 
+﻿Console.Write("Limit değerini giriniz: "); 
 int limit = Convert.ToInt32(Console.ReadLine());
 
-int sayac = 0;
+int sayac = 0; // başlangıç olduğu için 0 alıyoruz.
 
-while (sayac < limit)
+while (sayac < limit) //başlangıcı 0 aldığımız için sayacın limitten yalnızca küçük olduğu senaryoyu alıyoruz.
 {
     Console.WriteLine("Ben bir Patika'lıyım");
-    sayac++;
-}*/
+    sayac++; //sonsuz bir döngüde kalmaması için sayacı artırıyoruz ve limit değerine ulaştığında çıkmasını sağlıyoruz.
+}
+
+//while'da eğer 0'dan büyük değerler girersek koşul sağlanacak ve girdiğimiz değer kadar tanımladığımız yazı yazdırılacak.
+// Eğer limit'i -1 girersek sayac'tan küçük olacağı için koşul sağlanmayacak.
+
+
 
 Console.Write("Limit değerini giriniz: ");
-int limit = Convert.ToInt32(Console.ReadLine());
+int limit1 = Convert.ToInt32(Console.ReadLine());
 
-int sayac = 0;
+int sayac1 = 0; //başlangıç olarak 0 aldık.
 
 do
 {
-    Console.WriteLine("Ben bir Patika'lıyım");
-    sayac++;
-} while (sayac < limit);
+    Console.WriteLine("Ben bir Patika'lıyım"); //girdiğimiz limit değeri kadar sayıyı yazdıracak.
+    sayac1++; // sonsuz bir  döngüde kalmaması için limit değeri kadar sayacı artırıyor.
+} while (sayac1 < limit1); // eğer sayacın limitten küçük olduğu bir durum varsa yani koşul sağlanmasa da bir kere metni yazdırıyor.
+
 
 // FARK:
 // while döngüsünde koşul en başta kontrol edilir. Eğer limit -5 gibi bir negatif sayıysa, döngüye hiç girilmez.
